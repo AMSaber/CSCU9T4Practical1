8df4a1d57bc3c4f9047dbb7f8768a6d0c65874c4
AMSaber <106144659+AMSaber@users.noreply.github.com>
2023-03-31 23:55:46 +0400

    Add files via upload
    Steps 1-3 implemented.
    Added a button to GUI class for find all by date.
    Created a method to look up and output all entries on a specific date.
    Implemented functionality of the new lookup method to the newly created button

---
+++ TrainingRecord.java
@@ -0,0 +1,59 @@
+// An implementation of a Training Record as an ArrayList
+package com.stir.cscu9t4practical1;
+
+
+
+
+import java.util.*;
+
+
+public class TrainingRecord {
+    private List<Entry> tr;
+    
+    public TrainingRecord() {
+        tr = new ArrayList<Entry>();
+    } //constructor
+    
+    // add a record to the list
+   public void addEntry(Entry e){
+       tr.add(e);    
+   } // addClass
+   
+   // look up the entry of a given day and month
+   public String lookupEntry (int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "No entries found";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
+              result = current.getEntry();
+            }
+       return result;
+   } // lookupEntry
+   
+   //find all entries when given day, month and year
+    public String findAll (int d, int m, int y) {
+       ListIterator<Entry> iter = tr.listIterator();
+       String result = "";
+       while (iter.hasNext()) {
+          Entry current = iter.next();
+          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y){ 
+             result += current.getEntry();
+          }
+          else{
+              result = "No entries found";
+          }
+       }
+       return result;
+   } // Step 2
+   
+   // Count the number of entries
+   public int getNumberOfEntries(){
+       return tr.size();
+   }
+   // Clear all entries
+   public void clearAllEntries(){
+       tr.clear();
+   }
+   
+} // TrainingRecord
